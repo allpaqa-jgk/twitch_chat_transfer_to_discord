1bc482c7290879c5fec9e7433063a8f553447892
allpaqa-jgk <49287928+allpaqa-jgk@users.noreply.github.com>
2021-12-18 22:35:05 +0900

    update package file remove yarn remove forever, nexe and unused packages

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const { logger } = require("./src/logger");
 logger.info(`logger initialized level: ${logger.level}`);
 
-const forever = require("forever-monitor");
+// const forever = require("forever-monitor");
 const path = require("path");
-// require(path.join(__dirname, "./src/logger.js"));
 
 // init process.env
 process.env.NODE_CONFIG_DIR = path.join(__dirname, "./config");
@@ -12,13 +11,13 @@ logger.info("//////////////////////");
 logger.info("//  Ctrl-C to exit  //");
 logger.info("//////////////////////");
 
-const child = new forever.Monitor(path.join(__dirname, "./src/twitchBot.js"), {
-  //
-  // Options for restarting on watched files.
-  //
-  watch: process.env.NODE_ENV === "development", // Value indicating if we should watch files.
-  watchDirectory: "src", // Top-level directory to watch from.
-});
+// const child = new forever.Monitor(path.join(__dirname, "./src/twitchBot.js"), {
+//   //
+//   // Options for restarting on watched files.
+//   //
+//   watch: process.env.NODE_ENV === "development", // Value indicating if we should watch files.
+//   watchDirectory: "src", // Top-level directory to watch from.
+// });
 
 if (process.env.NODE_ENV === "development") {
   logger.debug("//////////////////////");
@@ -26,16 +25,18 @@ if (process.env.NODE_ENV === "development") {
   logger.debug("//////////////////////");
   // init child process of something.js
   // define events
-  child.on("watch:restart", function (info) {
-    logger.error("Restaring script because " + info.file + " changed");
-  });
+  // child.on("watch:restart", function (info) {
+  //   logger.error("Restaring script because " + info.file + " changed");
+  // });
 }
-child.on("restart", function () {
-  logger.error("Forever restarting script for " + child.times + " time");
-});
-child.on("exit:code", function (code) {
-  logger.error("Forever detected script exited with code " + code);
-});
+// child.on("restart", function () {
+//   logger.error("Forever restarting script for " + child.times + " time");
+// });
+// child.on("exit:code", function (code) {
+//   logger.error("Forever detected script exited with code " + code);
+// });
 
 // start process
-child.start();
+// child.start();
+
+require("./src/twitchBot.js");
